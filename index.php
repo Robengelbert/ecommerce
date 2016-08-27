7b6518aec8dcbb3dd52a18f2f8058ef2514614d3
Robert Engelbert <Robe32002@yahoo.com>
2016-08-27 09:09:34 -0600

    Created a  custom iterator by using iterator contract for Zend training

--- index.php
+++ index.php
@@ -1,9 +1,55 @@
-<!DOCTYPE html>
-	<html>
-	<head>
-		<title>Hello World!</title>
-	</head>
-	<body>
-		<h1>Hello World!</h1>
-	</body>
-</html>
+<?php
+
+/**
+ * Class elIterator
+ *
+ * @author Robert Engelbert <rob@robertengelbert.com>
+ */
+class elIterator implements Iterator{
+	private $location = 0;
+	private $list = array(
+		"locationOne",
+		"locationTwo",
+		"locationThree",
+	);
+
+	/**
+	 * elIterator constructor.
+	 */
+	public function __construct() {
+		$this->location = 0;
+	}
+
+	/**
+	 *  When you create a class and say that it implements Iterator, you must define the functions of Iterator.
+	 */
+
+	function current() {
+		var_dump(__METHOD__);
+		$this->$this->list[$this->location];
+	}
+	function key() {
+		var_dump(__METHOD__);
+		return $this->location;
+	}
+	function next() {
+		var_dump(__METHOD__);
+		++$this->location;
+	}
+	function rewind(){
+		var_dump(__METHOD__);
+		$this->location = 0;
+	}
+	function valid(){
+		var_dump(__METHOD__);
+		return isset($this->array[$this->location]);
+	}
+}
+
+$el = new elIterator;
+
+
+foreach($el as $key => $value){
+	var_dump($key, $value);
+	echo "\n";
+}

9c528b5527ddf9bd9ee4823e0e5b08f4c210d13b
Robert Engelbert <Robe32002@yahoo.com>
2016-10-01 13:25:45 -0600

    Added link to css in html and custom css

--- view/index.php
+++ view/index.php
@@ -3,21 +3,22 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Tiling images</title>
+		<link href="../css/style.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
 		<h1>Welcome to Balloons de Albuquerque</h1>
 
 		<section id="photos">
-			<img src="images/1darth_vader_balloon.jpg" alt="Darth vader balloon" />
-			<img src="images/angry_bird_balloon.jpg" alt="Angry bird balloon" />
-			<img src="images/beagle_maximus_balloon.jpg" alt="Beagle balloon" />
-			<img src="images/cosmos1_balloon.jpg" alt="Space man balloon" />
-			<img src="images/joelly_Bee_Balloon.jpg" alt="Bee balloon" />
-			<img src="images/mariachi_balloon.jpg" alt="Mariachi balloon" />
-			<img src="images/master_yoda_balloon.jpg" alt="Yoda balloon" />
-			<img src="images/pirate_ship_balloon.jpg" alt="Pirate ship balloon" />
-			<img src="images/rsz_creamland_cow_balloon.jpg" alt="cow balloon" />
-			<img src="images/sunny_boy_balloon.jpg" alt="sun balloon" />
+			<img src="../images/1darth_vader_balloon.jpg" alt="Darth vader balloon" />
+			<img src="../images/angry_bird_balloon.jpg" alt="Angry bird balloon" />
+			<img src="../images/beagle_maximus_balloon.jpg" alt="Beagle balloon" />
+			<img src="../images/cosmos1_balloon.jpg" alt="Space man balloon" />
+			<img src="../images/Joelly_Bee_Balloon.jpg" alt="Bee balloon" />
+			<img src="../images/mariachi_balloon.jpg" alt="Mariachi balloon" />
+			<img src="../images/master_yoda_balloon.jpg" alt="Yoda balloon" />
+			<img src="../images/pirate_ship_balloon.jpg" alt="Pirate ship balloon" />
+			<img src="../images/rsz_creamland_cow_balloon.jpg" alt="cow balloon" />
+			<img src="../images/sunny_boy_balloon.jpg" alt="sun balloon" />
 		</section>
 
 	</body>

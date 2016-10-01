6e6e04a1edf83e5f055d5b4d9053190292c43a2d
Robert Engelbert <Robe32002@yahoo.com>
2016-10-01 13:15:57 -0600

    Added 10 images and html

---
+++ view/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Tiling images</title>
+	</head>
+	<body>
+		<h1>Welcome to Balloons de Albuquerque</h1>
+
+		<section id="photos">
+			<img src="images/1darth_vader_balloon.jpg" alt="Darth vader balloon" />
+			<img src="images/angry_bird_balloon.jpg" alt="Angry bird balloon" />
+			<img src="images/beagle_maximus_balloon.jpg" alt="Beagle balloon" />
+			<img src="images/cosmos1_balloon.jpg" alt="Space man balloon" />
+			<img src="images/joelly_Bee_Balloon.jpg" alt="Bee balloon" />
+			<img src="images/mariachi_balloon.jpg" alt="Mariachi balloon" />
+			<img src="images/master_yoda_balloon.jpg" alt="Yoda balloon" />
+			<img src="images/pirate_ship_balloon.jpg" alt="Pirate ship balloon" />
+			<img src="images/rsz_creamland_cow_balloon.jpg" alt="cow balloon" />
+			<img src="images/sunny_boy_balloon.jpg" alt="sun balloon" />
+		</section>
+
+	</body>
+</html>

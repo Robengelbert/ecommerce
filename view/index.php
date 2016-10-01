aedd0277145065d2c0ce445288ce69dc62605ba0
Robert Engelbert <Robe32002@yahoo.com>
2016-10-01 14:04:23 -0600

    Added a link to bootstrap and jQuery

--- view/index.php
+++ view/index.php
@@ -3,7 +3,23 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Tiling images</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+		<!--		Custom css-->
 		<link href="../css/style.css" rel="stylesheet" type="text/css" />
+
+<!--		jQuery-->
+		<script
+			src="https://code.jquery.com/jquery-3.1.1.js"
+			integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+			crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 	</head>
 	<body>
 		<h1>Welcome to Balloons de Albuquerque</h1>
@@ -20,6 +36,31 @@
 			<img src="../images/rsz_creamland_cow_balloon.jpg" alt="cow balloon" />
 			<img src="../images/sunny_boy_balloon.jpg" alt="sun balloon" />
 		</section>
+		<div class="container">
+			<div class="row">
+				<p>How long does it take to make a special shapes balloon?</p>
+				<p class="hid" id="one">According to one manufacturer (Lindstrand Balloons USA) it takes 14-20 weeks after the design has been approved.</p>
+
+				<p>What do you have to do to be a pilot?</p>
+				<p class="hid" id="two">In the United States, a pilot of a hot air balloon must have a pilot certificate from the Federal Aviation Administration and it must carry the rating of "Lighter-than-air free balloon."</p>
+
+				<p>What's the longest hot air balloon flight?</p>
+				<p class="hid" id="three">On January 15, 1991, the Virgin Pacific Flyer balloon completed the longest flight in a hot air balloon when Per Lindstrand and Richard Branson flew 7,671.91 km (4,767.10 mi) from Japan to Northern Canada.
+				</p>
+
+				<p>What’s the highest flight?</p>
+				<p class="hid" id="four">On November 26, 2005 Vijaypat Singhania set the world altitude record for highest hot air balloon flight, reaching 21,027 m (68,986 ft).
+				</p>
+
+				<p>Why is ABQ such a good place for ballooning?</p>
+				<p class="hid" id="five">Wind currents and air temperature are key to the successful flying of hot air balloons. Albuquerque has the perfect combination of these conditions.
+				</p>
+
+				<p>Explain the ABQ box?</p>
+				<p class="hid" id="six">The Albuquerque “box” is a set of very predictable wind patterns that can be used to navigate balloons. At low elevations the winds tend to be southerly, but at higher elevations they tend to be northerly. Balloonists do not have a way to steer their hot air balloons but they can.use these “box” winds to move away from the field and back again.</p>
+
+			</div>
+		</div>
 
 	</body>
 </html>

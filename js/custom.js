cbe6fb7dae923c483898fd3d9a9c62b416a5ffeb
Robert Engelbert <Robe32002@yahoo.com>
2016-10-03 10:51:55 -0600

    Added jQuery to hide answers

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
-	$(".hid").hide();
-	$('#id').hover(
-		function(){$(this).show();}
-	);
-
+// $(document).ready(function(){
+// 	$(".hid").hide();
+// 	$('#id').hover(
+// 		function(){$(this).show();}
+// 	);
+//
+// });
+$(document).ready(function() {
+	$(".spoiler span").hide();
+	$(".spoiler").append("<button>Answer</button>");
+	$("button").click(function() {
+		$(this).prev().show();
+		$(this).remove();
+	});
 });
--- view/index.php
+++ view/index.php
@@ -42,29 +42,29 @@
 		<div class="container">
 			<div class="row">
 				<p>Q: How long does it take to make a special shapes balloon?</p>
-				<p class="hid" id="one">A: According to one manufacturer (Lindstrand Balloons USA) it takes 14-20 weeks after the design has been approved.</p>
+				<p class="spoiler"><span>A: According to one manufacturer (Lindstrand Balloons USA) it takes 14-20 weeks after the design has been approved.</span></p>
 
 				<p>Q: What do you have to do to be a pilot?</p>
-				<p class="hid" id="two">A: In the United States, a pilot of a hot air balloon must have a pilot certificate from the Federal Aviation Administration and it must carry the rating of "Lighter-than-air free balloon."</p>
+				<p class="spoiler"><span>A: In the United States, a pilot of a hot air balloon must have a pilot certificate from the Federal Aviation Administration and it must carry the rating of "Lighter-than-air free balloon."</span></p>
 
 				<p>Q: What's the longest hot air balloon flight?</p>
-				<p class="hid" id="three">A: On January 15, 1991, the Virgin Pacific Flyer balloon completed the longest flight in a hot air balloon when Per Lindstrand and Richard Branson flew 7,671.91 km (4,767.10 mi) from Japan to Northern Canada.
+				<p class="spoiler"><span>A: On January 15, 1991, the Virgin Pacific Flyer balloon completed the longest flight in a hot air balloon when Per Lindstrand and Richard Branson flew 7,671.91 km (4,767.10 mi) from Japan to Northern Canada.</span>
 				</p>
 
 				<p>Q: What’s the highest flight?</p>
-				<p class="hid" id="four">A: On November 26, 2005 Vijaypat Singhania set the world altitude record for highest hot air balloon flight, reaching 21,027 m (68,986 ft).
+				<p class="spoiler"><span>A: On November 26, 2005 Vijaypat Singhania set the world altitude record for highest hot air balloon flight, reaching 21,027 m (68,986 ft).</span>
 				</p>
 
 				<p>Q: What's the largest balloon every created?</p>
-				<p class="hid">A: With a volume of 74 thousand cubic meters (2.6 million cubic feet), the envelope of the Virgin Pacific Flyer balloon was the largest ever built for a hot air craft.
+				<p class="spoiler"><span>A: With a volume of 74 thousand cubic meters (2.6 million cubic feet), the envelope of the Virgin Pacific Flyer balloon was the largest ever built for a hot air craft.</span>
 				</p>
 
 				<p>Q: Why is ABQ such a good place for ballooning?</p>
-				<p class="hid" id="five">A:Wind currents and air temperature are key to the successful flying of hot air balloons. Albuquerque has the perfect combination of these conditions.
+				<p class="spoiler"><span>A:Wind currents and air temperature are key to the successful flying of hot air balloons. Albuquerque has the perfect combination of these conditions.</span>
 				</p>
 
 				<p>Q: Explain the ABQ box?</p>
-				<p class="hid" id="six">A: The Albuquerque “box” is a set of very predictable wind patterns that can be used to navigate balloons. At low elevations the winds tend to be southerly, but at higher elevations they tend to be northerly. Balloonists do not have a way to steer their hot air balloons but they can.use these “box” winds to move away from the field and back again.</p>
+				<p class="spoiler"><span>A: The Albuquerque “box” is a set of very predictable wind patterns that can be used to navigate balloons. At low elevations the winds tend to be southerly, but at higher elevations they tend to be northerly. Balloonists do not have a way to steer their hot air balloons but they can.use these “box” winds to move away from the field and back again.</span></p>
 
 			</div>
 		</div>

c93836568f083e131a0fadaaec091ebf214dde50
Robert Engelbert <Robe32002@yahoo.com>
2016-10-03 15:02:41 -0600

    Added jQuery to change color of navbar on scroll

--- js/custom.js
+++ js/custom.js
@@ -6,10 +6,21 @@
 //
 // });
 $(document).ready(function() {
+	var scroll_start = 0;
+	var startChange = $("#startChange");
+	var offset = startChange.offset();
+	$(document).scroll(function(){
+		scroll_start = $(this).scrollTop();
+		if(scroll_start > offset.top){
+			$("#navbar").css("background-color", "#ffff00");
+		}else{
+			$("#navbar").css("background-color", "transparent");
+		}
+	});
 	$(".spoiler span").hide();
 	$(".spoiler").append("<button>Answer</button>");
 	$("button").click(function() {
 		$(this).prev().show();
 		$(this).remove();
 	});
-});
+	});

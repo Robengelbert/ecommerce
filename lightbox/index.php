70062c08fcc12009d210b0a6c2ef4bed39adb4c6
Robert Engelbert <Robe32002@yahoo.com>
2016-10-04 11:49:56 -0600

    Added html and css for lightbox

---
+++ lightbox/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<link href="../css/main.css" rel="stylesheet" type="text/css"/>
+
+
+		<!--		jQuery-->
+		<script
+			src="https://code.jquery.com/jquery-3.1.1.js"
+			integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+			crossorigin="anonymous"></script>
+
+		<!--		custom jQuery-->
+		<script src="../js/app.js" type="text/javascript"></script>
+
+	</head>
+	<body>
+		<h1>Image Gallery</h1>
+		<ul id="imageGallery">
+			<li><a href="../images/1darth_vader_balloon.jpg"><img src="../images/1darth_vader_balloon.jpg" width="100"
+																					alt="Darth vader balloon"></a></li>
+			<li><a href="../images/angry_bird_balloon.jpg"><img src="../images/angry_bird_balloon.jpg" width="100"
+																				 alt="Angry bird balloon"></a></li>
+			<li><a href="../images/beagle_maximus_balloon.jpg"><img src="../images/beagle_maximus_balloon.jpg" width="100"
+																					  alt="Beagle maximus balloon"></a></li>
+			<li><a href="../images/cosmos1_balloon.jpg"><img src="../images/cosmos1_balloon.jpg" width="100"
+																			 alt="Cosmos balloon"></a></li>
+			<li><a href="../images/Joelly_Bee_Balloon.jpg"><img src="../images/Joelly_Bee_Balloon.jpg" width="100"
+																				 alt="Jolly bee balloon"></a></li>
+			<li><a href="../images/mariachi_balloon.jpg"><img src="../images/mariachi_balloon.jpg" width="100"
+																			  alt="Mariachi balloon"></a></li>
+			<li><a href="../images/master_yoda_balloon.jpg"><img src="../images/master_yoda_balloon.jpg" width="100"
+																				  alt="Master yoda balloon"></a></li>
+			<li><a href="../images/pirate_ship_balloon.jpg"><img src="../images/pirate_ship_balloon.jpg" width="100"
+																				  alt="Pirate ship balloon"></a></li>
+			<li><a href="../images/rsz_creamland_cow_balloon.jpg"><img src="../images/rsz_creamland_cow_balloon.jpg"
+																						  width="100" alt="Creamland cow balloon"></a></li>
+			<li><a href="../images/sunny_boy_balloon.jpg"><img src="../images/sunny_boy_balloon.jpg" width="100"
+																				alt="Sunny boy balloon"></a></li>
+		</ul>
+		<div id="overlay"></div>
+	</body>
+</html>
